4e90743aa26564069eec68898820ff9910212306
Egehan KAHRAMAN <vastseaoffical0@outlook.com>
2026-08-15 00:17:54 +0300

    feat(site): support dynamic base path and seamless portfolio hosting

--- www/site/astro.config.mjs
+++ www/site/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://huma-lang.org',
+  base: process.env.ASTRO_BASE !== undefined ? process.env.ASTRO_BASE : '/projects/huma-lang',
   output: 'static'
 });
